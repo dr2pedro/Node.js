079f7b666eb3524a9623bdc1f9425d491af82fc5
Pedro Paulo dos Santos <dr2p@hotmail.com>
2020-09-12 11:09:54 -0300

    Update routes.js

--- server/src/signIn/src/routes.js
+++ server/src/signIn/src/routes.js
@@ -1,11 +1,6 @@
 const express = require('express')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-
-const user = db.get('users')
 
 const router = express.Router()
-const {secret} = require('../auth.json')
 
 router.get('/', function (req, res) {
   res.send('Welcome')
