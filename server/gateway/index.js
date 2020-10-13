aa3a530a8548c2ce18f1a3594c9cfc50eeaf260f
Pedro (W) <dr2p@hotmail.com>
2020-10-13 02:47:37 -0300

    parametrizando

--- server/gateway/index.js
+++ server/gateway/index.js
@@ -7,19 +7,21 @@ const morgan = require('morgan')
 
 require('dotenv').config()
 
-const port = process.env.PORT_GATEWAY || 5080
- 
+const gateway_port = process.env.PORT_GATEWAY || 5080
+const signin_port = process.env.PORT_SIGNIN
+const signup_port = process.env.PORT_SIGNUP
+
 app.use(morgan('dev'))
 app.use(helmet())
 app.use(express.json())
 app.use(cors())
 
-app.use('/signin', proxy('http://signin:2221/'))
-app.use('/signup', proxy('http://signup:2222/'))
+app.use('/signin', proxy('http://signin:'+ signin_port + '/'))
+app.use('/signup', proxy('http://signup' + signup_port + '/'))
 
 
 app.listen(port, () => {
   /* eslint-disable no-console */
-  console.log(`Gateway endpoints listening on: http://localhost:${port}`)
+  console.log(`Gateway endpoints listening on: http://localhost:${gateway_port}`)
   /* eslint-enable no-console */
 })
