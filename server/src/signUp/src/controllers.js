55e743a91dc1d2439f42aebf6576780be1d9382c
Pedro (W) <dr2p@hotmail.com>
2020-10-18 00:21:19 -0300

    keep going...

--- server/src/signUp/src/controllers.js
+++ server/src/signUp/src/controllers.js
@@ -1,41 +1,42 @@
-const monk = require('monk'),
-      bcrypt = require('bcrypt'),
-      jwt = require('jsonwebtoken'),
-      db = monk(process.env.MONGO_URI),
-      user = db.get('users'),
-      { secret } = require('../auth.json'),
-      schema = require('./schema.js')
+const 
+    monk = require('monk'),
+    bcrypt = require('bcrypt'),
+    jwt = require('jsonwebtoken'),
+    db = monk(process.env.MONGO_URI),
+    user = db.get('users'),
+    { secret } = require('../auth.json'),
+    schema = require('./schema.js')
 
-exports.validateRequest = ( async ( req, res, next ) => {
-    try {
-        await schema.validateAsync(req.body)
-        next()
-    } catch (error) {
-        res.status(400).send(error.message)
-    }     
-})
+exports
+    .validateRequest = ( async ( req, res, next ) => {
+        try {
+            await schema.validateAsync(req.body)
+            next()
+        } catch (error) {
+            res.status(400).send(error.message)
+        }     
+    })
 
-exports.findUser = ( async ( req, res, next ) => {
-    try {  
-        const { email } = req.body
-        const payload = await user.findOne({ email })
-        if (payload) { return res.status(409).send({ error: 'User already exists!' }) }
-        next()
-    } catch (error) {
-        return res.status(400).send(error.message)
-    } 
-})
+    .findUser = ( async ( req, res, next ) => {
+        try {  
+            const { email } = req.body
+            const payload = await user.findOne({ email })
+            if (payload) { return res.status(409).send({ error: 'User already exists!' }) }
+            next()
+        } catch (error) {
+            return res.status(400).send(error.message)
+        } 
+    })
 
-exports.createUser = ( async (req, res) => {
-    try {       
-        password = await bcrypt.hash(req.body.password, 10)
-        req.body.password = password
-        const payload = await user.insert(req.body)
-        payload.password = undefined
-        const token = jwt.sign({ _id: payload._id, email: payload.email }, secret, { expiresIn: 14400 })
-        return res.status(201).json({ payload, token })
-    } catch (error) {
-        return res.status(400).send(error.message) 
-    }
-})
-
+    .createUser = ( async (req, res) => {
+        try {       
+            password = await bcrypt.hash(req.body.password, 10)
+            req.body.password = password
+            const payload = await user.insert(req.body)
+            payload.password = undefined
+            const token = jwt.sign({ _id: payload._id, email: payload.email }, secret, { expiresIn: 14400 })
+            return res.status(201).json({ payload, token })
+        } catch (error) {
+            return res.status(400).send(error.message) 
+        }
+    })
--- server/src/signUp/src/schema.js
+++ server/src/signUp/src/schema.js
@@ -1,6 +1,8 @@
-const joi = require('@hapi/joi')
+const 
+    joi = require('@hapi/joi')
 
-exports.manifest_schema = joi.object({
-  email: joi.string().trim().email().required(),
-  password: joi.string().trim().required()
-})
+exports
+  .manifest_schema = joi.object({
+      email: joi.string().trim().email().required(),
+      password: joi.string().trim().required()
+  })
