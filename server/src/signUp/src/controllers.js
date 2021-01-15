949d389a7d8ee84770b0626dae0284728b8df6fb
Pedro (W) <dr2p@hotmail.com>
2021-01-14 22:08:38 -0300

    ...

--- server/src/signUp/src/controllers.js
+++ server/src/signUp/src/controllers.js
@@ -1,42 +1,46 @@
 const 
     monk = require('monk'),
     bcrypt = require('bcrypt'),
-    jwt = require('jsonwebtoken'),
-    joi = require('joi')
+    jwt = require('jsonwebtoken')
 
 
 module.exports = {
     
-    validateRequest: ( async (  req, res, next, schema ) => {
-       try{
-            await schema.validateAsync(req.body)
-            return next()
-        } catch (error){
-            return res.status(400).send(error.message)
-        }                
+    validateRequest: ( ( schema ) => {
+        return async (req, res, next) => { 
+            try{
+                await schema.validateAsync(req.body)
+                next()
+            } catch (error){
+                res.status(400).send(error.message)
+            }        
+        }       
     })
     ,
-    selectColletion: ( (req, res, next, URI, name) => {
-        try{
-            const collection = monk(URI).get('name')
-            return {collection, next()}
-        } catch (error){
-            return res.status(400).send(error.message)
-        }                
+    selectCollection: ( ( URI, name ) => {
+        return ( _req, res, next ) => {  
+            try{
+                const collection = monk(URI).get(name)
+                next()
+            } catch (error){
+                res.status(400).send(error.message)
+            }     
+        }   
     })
     ,
-    findUser: ( async (  req, res, next, collection, noUser=true, key) => {
-        try {  
-            const 
-                { key } = req.body,
-                payload = await colletion.findOne({ key })
-            
-            return payload & noUser=true ? res.status(409).send({ error: 'User already exists!' })
-                   : noUser=false & !payload ? res.status(404).send({ error: 'User not found!' })
-                   : next()
-            
-        } catch (error) {
-            return res.status(400).send(error.message)
+    findUserByEmail: ( ( collection, noUser=true ) => {
+        return async (req, res, next) => {
+            try {
+                const { email } = req.body
+                payload = await collection.findOne({ email })
+
+                payload & noUser == true ? res.status(409).send({ error: 'User already exists!' })
+                    : noUser == false & !payload ? res.status(404).send({ error: 'User not found!' })
+                        : next()
+
+            } catch (error) {
+                res.status(400).send(error.message)
+            }
         } 
     })
     ,
--- server/src/signUp/src/routes.js
+++ server/src/signUp/src/routes.js
@@ -1,11 +1,18 @@
 const 
     express = require('express'),
-    { validateRequest, findUser, createUser } = require('./controllers'),
-    router = express.Router()
+    { validateRequest, selectCollection ,findUserByEmail, createUser } = require('./controllers'),
+    router = express.Router(),
+    URI = monk(process.env.MONGO_READ_URI),
+    name = 'users',
+    joi = require('joi')
+
+
+const schema = joi.object({ email: joi.string().trim().email().required(),
+                            password: joi.string().trim().required() })
 
 router
     .get('/', (req, res) => {res.send('The signup app is healthy...')})
-    .post('/', [validateRequest, findUser, createUser])
+    .post('/', [validateRequest, selectCollection, findUserByEmail, createUser])
 
     
 module.exports = router
